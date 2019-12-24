4d99b50031c3fabf5eda573bf999a659b392d477
informathemusic <39065949+informathemusic@users.noreply.github.com>
2019-12-24 20:09:47 +0100

    Update index.js

--- blocklingnews/src/index.js
+++ blocklingnews/src/index.js
@@ -92,7 +92,7 @@ xhr.onload = async function() {
   document.body.style.marginLeft = "1vw";
   document.body.style.opacity = "1";
   document.getElementById("waiting").style.opacity = "0";
-  document.getElementById("headerimg").style.margin = "50vw";
+  document.getElementById("headerimg").style.marginLeft = "50vw";
   document.getElementById("content").style.margin = "1vw";
   //https://jsonstorage.net/api/items/6618ee1d-f14c-4c17-8691-ff17652e7668
   //'https://www.random.org/integers/?num='5'&min=0&max='100'&col='8'&base=10&format=plain&rnd=new'
