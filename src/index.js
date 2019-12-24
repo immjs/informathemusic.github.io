6f9e81aa326ad641bd9ab1953b7d9358d7a93dca
informathemusic <39065949+informathemusic@users.noreply.github.com>
2019-12-24 10:08:45 +0100

    a
    a

--- src/index.js
+++ src/index.js
@@ -21,20 +21,47 @@ function getRandomInt(min, max) {
   return n === genN ? arr : levelNRandomness(n++, min, max, arr, genN);
 }
 console.log(levelNRandomness(6,0,10));*/
+
+var waitForLoad = id => {
+  return new Promise((resolve, reject) => {
+    var tXhr = new XMLHttpRequest();
+    tXhr.onerror = () => {
+      document.body.innerHTML +=
+        '<h3 id="errorLoading">Could not reach youtube </h3>';
+      reject();
+    };
+    tXhr.onload = async function() {
+      resolve(this.response);
+    };
+    tXhr.open(
+      "GET",
+      "https://www.googleapis.com/youtube/v3/videos?part=snippet&id=" +
+        id +
+        "&key=AIzaSyCLCw6F8TbQUkRY15uFaZCQvsfrn4mvMnI",
+      true
+    );
+    tXhr.send(null);
+  });
+};
 var xhr = new XMLHttpRequest();
-var dom = "https://api.npont.io/c9cd93525cf4fdc39073";
+var dom = "https://api.npoint.io/c9cd93525cf4fdc39073";
 xhr.open("GET", dom, true);
 xhr.onerror = () => {
   return (document.body.innerHTML +=
     '<h3 id="errorLoading">Could not reach ' + dom + "</h3>");
 };
-xhr.onload = function() {
+xhr.onload = async function() {
   if (this.status !== 200) {
     return (document.body.innerHTML = "<h1>Could not reach" + dom + "</h1>");
   }
   var data = JSON.parse(this.response);
   let k = data.length;
   for (var i = k; i > 0; i--) {
+    data[i - 1].id = JSON.parse(await waitForLoad(data[i - 1].id));
+    data[i - 1].id = data[i - 1].id.items[0].snippet.title;
+    console.log(data[i - 1].id, i - 1);
+  }
+  for (i = k; i > 0; i--) {
     var p = document.createElement("div");
     content.appendChild(p);
     p.outerHTML =
@@ -47,7 +74,7 @@ xhr.onload = function() {
       "/article" +
       i +
       '.html" class="artHeadlink">\n<h2 class="z">' +
-      data[i - 1].title +
+      data[i - 1].id +
       '<h6 class="seen z" style="opacity:0"><span class="seenchild">Deja vu</span></h6></h2>\n</a>\n<a href="/Articles/article' +
       i +
       "/article" +
@@ -55,7 +82,6 @@ xhr.onload = function() {
       '.html" class="artContent">\n<p>' +
       data[i - 1].summary.split("\n").join("<br>") +
       "</p>\n</a>\n</div>\n</div>";
-    console.log(content);
   }
   document.body.style.marginLeft = "1vw";
   document.body.style.opacity = "1";
