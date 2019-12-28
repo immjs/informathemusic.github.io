0a3e4025c355ca5b4ed592393da079427058f4bf
informathemusic <39065949+informathemusic@users.noreply.github.com>
2019-12-28 14:21:20 +0100

    Update index.js

--- blocklingnews/src/index.js
+++ blocklingnews/src/index.js
@@ -69,31 +69,12 @@ xhr.onload = async function() {
     var p = document.createElement("div");
     content.appendChild(p);
     p.outerHTML =
-      '<div class="sumo">\n<div class="article" style="background-color:' +
-      colors[r] +
-      ';box-shadow: 1vw 1vw '+
-      uColors[r]+
-      ';"><div class="x">\n<h1>' +
-      (i === k ? "\nNouveau:\n" : "") +
-      '</h1>\n<a href="/Articles/article' +
-      i +
-      "/article" +
-      i +
-      '.html" class="artHeadlink">\n<h2 class="z">' +
-      data[i - 1].id +
-      '<h6 class="seen z" style="opacity:0"><span class="seenchild">Deja vu</span></h6></h2>\n</a>\n<a href="/Articles/article' +
-      i +
-      "/article" +
-      i +
-      '.html" class="artContent">\n<p>' +
-      data[i - 1].summary.split("\n").join("<br>") +
-      "</p>\n</a>\n</div>\n</div>\n</div>";
+      
+        '<article class="article">'+
+            '<h3 class="article__title">'+data.id+'</h3>'+
+            '<p class="article__content">'+data.summary+'</p>'+
+        '</article>'
   }
-  document.body.style.transform = "translate(1vw)";
-  document.body.style.opacity = "1";
-  document.getElementById("waiting").style.opacity = "0";
-  document.getElementById("headerimg").style.transform = "translateX(0vw)";
-  document.getElementById("content").style.transform = "translateX(50vw)";
   //https://jsonstorage.net/api/items/6618ee1d-f14c-4c17-8691-ff17652e7668
   //'https://www.random.org/integers/?num='5'&min=0&max='100'&col='8'&base=10&format=plain&rnd=new'
 };
